c4f46346614ac281aac11cd2b81839d9b3087ba9
yeh-George <ycj012@163.com>
2019-12-24 10:31:37 +0800

    day01 make a webapp bone

---
+++ www/app.py
@@ -0,0 +1,21 @@
+﻿import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'Awesome')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server start at http://127.0.0.1:9000...')
+    return srv
+    
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
+    
